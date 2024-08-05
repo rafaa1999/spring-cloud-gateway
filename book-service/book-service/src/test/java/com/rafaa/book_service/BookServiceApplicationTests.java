79d2f5db4044a8f702636676ba074c149c8e7337
rafaa1999 <raferhouma10@gmail.com>
2024-08-05 16:18:15 +0100

    Test /books endpoint:
    - when get requests then return books

--- book-service/book-service/src/test/java/com/rafaa/book_service/BookServiceApplicationTests.java
+++ book-service/book-service/src/test/java/com/rafaa/book_service/BookServiceApplicationTests.java
@@ -1,15 +1,30 @@
 package com.rafaa.book_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void when_get_requests_then_return_books(){
+		webTestClient
+				.get()
+				.uri("/books")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBodyList(Book.class)
+				.contains(
+						new Book("Harry Potter"),
+						new Book("His Dark Materials"),
+						new Book("The Hobbit"),
+						new Book("The Lord of the Rings")
+				);
 	}
 
 }
