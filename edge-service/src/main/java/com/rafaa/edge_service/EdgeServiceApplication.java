51c0facb3f1421d4072827e086995783b053e816
rafaa1999 <raferhouma10@gmail.com>
2024-08-07 10:43:05 +0100

    Implement resilience4j

--- edge-service/src/main/java/com/rafaa/edge_service/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/rafaa/edge_service/EdgeServiceApplication.java
@@ -2,12 +2,33 @@ package com.rafaa.edge_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver keyResolver() {
+		return exchange -> Mono.just("ANONYMOUS");
+	}
+
+}
+
+@RestController
+class FallbackController {
+
+	@GetMapping("/books-fallback")
+	Flux<Void> getBooksFallback() {
+		return Flux.empty();
 	}
 
 }
